d97843a3cae93912ed2c84c6e2274b9593846d76
seong in Hong <75526895+espada105@users.noreply.github.com>
2023-10-30 17:36:30 +0900

    1

--- 2675.py
+++ 2675.py
@@ -1,6 +1,8 @@
-R = int(input())
-S = list(input())
-P=[]
-for i in range(R):
-    P.append(S[i]*R)
-print(P)    
+T = int(input())
+
+for i in range(T):
+    R, S = input().split()
+    P = []
+    for i in range(0, len(S)):
+        P.append(S[i] * int(R))
+    print(''.join(P))
