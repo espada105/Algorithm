c4de35b9985c856d9572c633a6e59befd4cb4035
seong in Hong <75526895+espada105@users.noreply.github.com>
2025-08-07 11:28:22 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,18 @@
+score = 0
+num = 0
+
+grade_score = {
+    'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0,
+    'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0
+}
+
+for i in range(20):
+    subject = input().split()
+    if subject[2] != 'P':
+        credit = float(subject[1])
+        grade = subject[2]
+        
+        num += credit
+        score += credit * grade_score[grade]
+
+print(score / num)
